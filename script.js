49235fc9933def77851884cdd045f413b7085408
maimeow <126539340+mainehartily@users.noreply.github.com>
2024-05-29 17:43:06 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+/* Collabsible Navbar */
+function openNav() {
+    document.getElementById("mySidebar").style.right = "0";
+  }
+
+  function closeNav() {
+    document.getElementById("mySidebar").style.right = "-250px";
+  }
